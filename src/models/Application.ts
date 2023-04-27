f35aebb0e56e845f32ee6d69a1578c13a895a0ac
Mariano Hurtado de Mendoza <theunfactoringguru@gmail.com>
2023-04-27 12:59:11 -0600

    Developed server

--- src/models/Application.ts
+++ src/models/Application.ts
@@ -5,6 +5,7 @@ const Application = new mongoose.Schema({
   applicationId: {type: String, required: true, default: ''},
   name: {type: String, required: true, default: ''},
   order: {type: Types.ObjectId, ref: 'Order'},
+  status: {type: String, default: 'En Reunion'},
   services: [{type: Types.ObjectId, ref: 'Service'}],
   description: {type: String, required: true},
 })
